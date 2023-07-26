48cc57dc9b152c6f60433cad32e9e68ef7f4cda5
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-07-26 01:37:34 -0400

    Added basic leva gui helper

--- src/App/App.js
+++ src/App/App.js
@@ -3,20 +3,28 @@ import { CameraControls, PerspectiveCamera } from '@react-three/drei'
 // import { Physics } from "@react-three/rapier";
 import { useRef } from "react";
 import World from "./World";
+import { useControls } from 'leva'
+
 
 const App = () => {
   const perspectiveCamera = useRef(null);
-
+  const constrols = useControls('Camera', {
+    fov: {
+      value: 75,
+      min: 13,
+      max: 100,
+      step: 1,
+    }
+  })
   return (
     <Canvas
       args={[window.innerWidth, window.innerHeight]}
-      camera={{ fov: 75 }}
       dpr={[1, 1.5]}
       resize={true}
       shadows
     >
       {/* <Physics debug> */}
-        <PerspectiveCamera ref={perspectiveCamera} fov={75} position={[0, 10, 10]} makeDefault={true} />
+        <PerspectiveCamera ref={perspectiveCamera} fov={constrols.fov} position={[0, 10, 10]} makeDefault={true} />
         <World />
         <CameraControls />
       {/* </Physics> */}
